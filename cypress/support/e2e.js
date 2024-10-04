392f92172ef1a07d10a0c981e5efbd6a1b70c899
Samanweeta29 <mohammad.fareed@joshsoftware.com>
2024-10-04 11:12:39 +0530

    added reporting

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -25,3 +25,5 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from failing the test
     return false;
   });
+
+  import 'cypress-mochawesome-reporter/register';
